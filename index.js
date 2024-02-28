9b546cfdd72151876300d28a2ee4346447dcec60
AliakseiMakarankau <amakarankau@infoblox.com>
2024-02-28 11:04:09 +0200

    upd(task1): minor fixes

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const max = 1000;
 export default function getRandomNumber() {
   return Math.floor((Math.random() * (max - min + 1)) + min);
 }
+
+let result = getRandomNumber();
+console.log(result);
